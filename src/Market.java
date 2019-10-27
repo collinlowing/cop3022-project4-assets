e2e0babda03af46ee6669e822eeaeaa4e4f40fbf
Collin Lowing <cel19@students.uwf.edu>
2019-10-27 00:29:59 -0500

    Implemented Market

--- src/Market.java
+++ src/Market.java
@@ -1,11 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Market 
 {
-
-	public static void main(String[] args) 
+	private Asset [] assets;
+	private Asset [] invest;
+	private String fileName;
+	
+	public Market(String fileName)
 	{
+		this.fileName = fileName;
+	}
+	
+	public void readFile() throws Exception
+	{
+		String line;
+		try(BufferedReader reader = new BufferedReader(new FileReader(fileName)))
+		{
+			while((line = reader.readLine()) != null)
+			{
+				String [] assetData = line.split(",");
+				//if()
+				//{
+					// 
+				//}
+			}
+			reader.close();
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getClass().getName());
+			System.out.println(e.getMessage());
+		}
 		
 	}
 	
+	public String printAssets()
+	{
+		return "";
+	}
 	
+	public String toString()
+	{
+		return "";
+	}
 }
